58a4bf6b9b171e54f444acf76f4f504e58f3350f
sridevi <srivisakha2017@gmail.com>
2025-08-01 15:27:37 +0530

    added requirements txt file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot_llm",
+    version="0.1.0",
+    author="Sridevi",
+    author_email="sri@sri.com",
+    packages=find_packages(),
+    install_requires=[]
+)
